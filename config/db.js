8960c74150b676f8e3b993707c560c0b4270a41d
BHARGAV <91306528+code-king018@users.noreply.github.com>
2023-02-17 13:45:30 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect("mongodb+srv://USERNAME:USERNAME1@cluster0.ixcqetw.mongodb.net/test");
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`Mongodb Server Issue ${error}`.bgRed.white);
